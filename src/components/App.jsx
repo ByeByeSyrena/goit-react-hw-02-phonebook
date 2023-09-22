2981eaa72f7fc6ac255c373d76dfd853a471dcef
Ira S <iryna.shevchenko2@gmail.com>
2023-09-22 22:57:01 +0300

    filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,17 @@ export class App extends Component {
   state = {
     name: '',
     number: '',
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
   handleChange = (event) => {
-    event.preventDefault();
     const { name, value } = event.target;
-    // console.log(event.currentTarget.value);
     this.setState({ [name]: value });
   };
 
@@ -40,48 +44,72 @@ export class App extends Component {
     });
   };
 
-
   render() {
-    const { name, number } = this.state;
+  const { name, number, filter, contacts } = this.state;
 
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor={this.loginInputId}>Phonebook</label>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash, and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            onChange={this.handleChange}
-            value={name}
-            id={this.loginInputId}
-          />
+  // Filter the contacts based on the user's input
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
-          <label htmlFor={this.loginInputId}>Number</label>
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            onChange={this.handleChange}
-            value={number}
-            id={this.loginInputId}
-          />
+  return (
+    <>
+      <form onSubmit={this.handleSubmit}>
+        <h1>Phonebook</h1>
+        <input
+          type="text"
+          name="name"
+          title="Name may contain only letters, apostrophe, dash, and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={this.handleChange}
+          value={name}
+          id={this.loginInputId}
+        />
 
-          <button type="submit">Add contact</button>
-        </form>
-        <h1>Contacts</h1>
+        <h3>Number</h3>
+        <input
+          type="tel"
+          name="number"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={this.handleChange}
+          value={number}
+          id={this.loginInputId}
+        />
+
+        <button type="submit">Add contact</button>
+      </form>
+      <h2>Contacts</h2>
+      <h3>Find contacts by name</h3>
+      <input
+        type="text"
+        name="filter"
+        onChange={this.handleChange}
+        value={filter}
+        id={this.loginInputId}
+      />
+
+      {filter === '' ? (
         <ul>
-          {this.state.contacts.map((contact) => (
+          {contacts.map((contact) => (
             <li key={contact.id}>
               {contact.name}: {contact.number}
             </li>
           ))}
         </ul>
-      </>
-    );
-  }
+      ) : filteredContacts.length > 0 ? (
+        <ul>
+          {filteredContacts.map((contact) => (
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No matching contacts found</p>
+      )}
+    </>
+  );
+}
+
 }
